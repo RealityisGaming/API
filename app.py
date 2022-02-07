9fe40b0a53ffc0c05449b5114b2e5c9d74bee235
Matthew <matthew.hoecker1@gmail.com>
2022-02-07 15:03:57 -0600

    New Guide

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
@@ -28,6 +28,21 @@ class GuideSchema(ma.Schema):
 
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
+
+#end point to create a new guide
+@app.route('/guide', mthods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(titel, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide.schema.jsonify(guide)
 
 
 if __name__ == '__main__':
